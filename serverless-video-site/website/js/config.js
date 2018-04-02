c559fb6e8dd539e80eada052fa006770a4dcccd2
Sanjay Rawat <SANJAYRA@amdocs.com>
2018-04-02 17:03:18 -0500

    serverless website

---
+++ serverless-video-site/website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'makemenerd.auth0.com',
+        clientId: 'iRB6e2T20kUMFnlNHEO0qgU52gHLbdoe'
+    },
+    apiBaseUrl: 'https://zngubja40h.execute-api.us-east-1.amazonaws.com/dev'
+};
