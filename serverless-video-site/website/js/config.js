7b42c51bb44824febb636daf763da401ee9528e9
Sanjay Rawat <sanjayrawat177@gmail.com>
2018-04-02 17:15:24 -0500

    Update config.js

--- serverless-video-site/website/js/config.js
+++ serverless-video-site/website/js/config.js
@@ -1,7 +1,7 @@
 var configConstants = {
     auth0: {
-        domain: 'makemenerd.auth0.com',
-        clientId: 'iRB6e2T20kUMFnlNHEO0qgU52gHLbdoe'
+        domain: '<domain>',
+        clientId: '<cientid>'
     },
-    apiBaseUrl: 'https://zngubja40h.execute-api.us-east-1.amazonaws.com/dev'
+    apiBaseUrl: '<baseur>'
 };
